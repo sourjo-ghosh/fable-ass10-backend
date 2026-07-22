6443c19b0391a5bfa52b086f708ffb83f30b0db7
sourjo-ghosh <joyghosh211@outlook.com>
2026-07-22 08:11:13 +0600

    refactor: improve MongoDB client initialization and connection handling

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
-const  {MongoClient} = require ('mongodb'); 
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const dns = require("node:dns");
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
-
-
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
 dotenv.config();
-const app = express()
-const port = 3001
+const app = express();
+const port = 3001;
+// const client = new MongoClient();
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-
-const client = new MongoClient(process.env.MONGODB_URI);
-
-async function connectToMongoDB() {
+async function run() {
   try {
-    await client.connect();
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const db = await client.db("fable");
+    const allUser = await db.collection("user").find().toArray();
+    console.log(allUser);
     console.log("You successfully connected to MongoDB!");
-    return client;
+    app.get("/", (req, res) => {
+      res.send(allUser);
+    });
   } catch (err) {
     console.dir(err);
   }
 }
 
 // Call this only when your application terminates
-async function disconnectFromMongoDB() {
-  await client.close();
-}
-
-connectToMongoDB().catch(console.dir);
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// async function disconnectFromMongoDB() {
+//   await client.close();
+// }
+run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
