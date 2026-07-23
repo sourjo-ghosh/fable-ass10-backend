034e14d6846878572493b46446bb477699300e0d
sourjo-ghosh <joyghosh211@outlook.com>
2026-07-23 17:28:52 +0600

    feat: add CORS support and two api 1. all-ebook for all book and add-ebook for adding new book

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
 const dns = require("node:dns");
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
-const port = 3001;
+app.use(cors());
+app.use(express.json());
+const port = 8000;
 // const client = new MongoClient();
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, {
@@ -21,11 +24,28 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     const db = await client.db("fable");
-    const allUser = await db.collection("user").find().toArray();
+    const allUser = await db.collection("user");
+    const allEbook = await db.collection("ebook");
+    // const allEbookCollection = await allEbook.find({}).toArray();
     console.log(allUser);
+    console.log(allEbook);
     console.log("You successfully connected to MongoDB!");
     app.get("/", (req, res) => {
       res.send(allUser);
+    });
+    app.post("/api/add-ebook", async (req, res) => {
+      const ebookData = req.body;
+      if (!ebookData) {
+        return res.status(400).send("Ebook data is required");
+      }
+      const result = await allEbook.insertOne(ebookData);
+      res.json({ success: true, message: "Ebook data inserted successfully", result });
+      console.log(result);
+    });
+    app.get("/api/all-ebook", async (req, res) => {
+      const ebookData = await allEbook.find({}).toArray();
+      res.json({ success: true, message: "Ebook data retrieved successfully", result: ebookData });
+      // console.log(result);
     });
   } catch (err) {
     console.dir(err);
